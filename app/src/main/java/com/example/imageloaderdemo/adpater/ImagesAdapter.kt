8fcf0f483ce315bbc6560ad348bf67e3da83e1d0
wengong.xiong <wengong.xiong@thoughtwork.com>
2022-03-25 16:31:24 +0800

    test jenkins

--- app/src/main/java/com/example/imageloaderdemo/adpater/ImagesAdapter.kt
+++ app/src/main/java/com/example/imageloaderdemo/adpater/ImagesAdapter.kt
@@ -48,10 +48,7 @@ class ImagesAdapter : RecyclerView.Adapter<ImagesAdapter.ImageHolder>() {
                 .placeholder(R.mipmap.placeholder)
                 .into(it)
 
-//            Glide.with(it.context)
-//                .load(url)
-//                .into(it)
-        }
+      }
     }
 
     inner class ImageHolder(view: View) : RecyclerView.ViewHolder(view)
